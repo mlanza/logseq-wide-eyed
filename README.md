331b45ade0817f35f289d44d49dde4a6e3ba317d
Mario T. Lanza <mlanza@doesideas.com>
2022-08-08 18:59:24 -0400

    use style carousel internals

--- README.md
+++ README.md
@@ -2,44 +2,7 @@
 
 Toggles the visibility of completed and canceled to-dos.
 
-Toggling is controlled by the eye appearing in the toolbar.  It has opened and closed states which determine the appearance of matching blocks.  This is useful for showing/hiding content such as completed/canceled to-dos, but it can be configured for other purposes.
-
-If any matching content is found on the page or its linked references the eye is underlined.
-
-The opened styling rules are in effect only while hovering over the page body.
-
-[Style Carousel](https://github.com/mlanza/logseq-style-carousel) is more robust, has the same default behavior—toggling the visibility of completed and canceled to-dos—and is, in many situations, more effective.
-
-## Configurable Settings
-* `status` — a preferred initial status of `opened` or `closed`
-* `match` — a regular expression string which matches against text content
-* `closed` — stylesheet rules applied against matched blocks when the eye is closed
-* `opened` — stylesheet rules applied against matched blocks when the eye is opened
-* `targets` — what contexts are evaluated?
-* `refreshRate` — how often in seconds to check for changes to current page?  0 to disable
-
-## Default Settings
-* `status` — "closed"
-* `match` — "(^DONE|^CANCELED) "
-* `closed` — "display: none;"
-* `opened` — "text-decoration: underline wavy;"
-* `targets` — ["journals", "page-body", "linked-refs"]
-* `refreshRate` — 5
-
-## Design Notes
-The primary difference between Wide Eyed and [Style Carousel](https://github.com/mlanza/logseq-style-carousel) is the manner of content matching.
-Wide Eyed queries the page as it attempts to find matching blocks, but the main Logseq UI is multifaceted (e.g. Linked References, Unlinked References, embedded queries, some blocks lazily load) and Wide Eyed can neither anticipate nor easily see everything.  Since the Logseq Plugin api does not provide [sufficient hooks](https://discuss.logseq.com/t/add-more-event-hooks-subscriptions-for-plugins/5508) and [rendering events](https://discuss.logseq.com/t/add-plugin-rendering-pipeline/5549) the problem [cannot be easily overcome](https://github.com/mlanza/logseq-style-carousel/issues/1#issuecomment-1049810662).
-
-Style Carousel does not attempt to discern what can be found on the page.  Rather it applies style rules to achieve similar effect.  Furthermore, it can optionally employ queries.  The difference is, when it does, it does not attempt to discern what can be found on the page, but rather builds style rules based on queries against the complete graph.  These style rules, while encompassing a wider scope, are effective in the context of any page.  In most cases, Style Carousel will get you better results.
-
-While both plugins by default toggle TODO visibility, it was never meant as their sole purpose.  Rather it was meant to offer an example of what's possible.  The purpose is to allow you to configure buttons which conditionally find and style content in whatever manner you dream up.
-
-## Manual installation
-* Download this repo
-* In Logseq:
-  * Ensure Developer Mode is on
-  * Open Plugins
-  * Select `Load unpacked plugin`
+Version 2 replaced the guts with the internals of [Style Carousel](https://github.com/mlanza/logseq-style-carousel).  The prior implementation is no longer supported.  The only difference in Wide Eyed is the icon font set used for the ever-present eye.
 
 ## License
 [MIT](./LICENSE.md)
--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-const config = {};
-
-const state = {
-  ids: [],
-  status: "closed",
-  edits: []
-}
-
-function getIds(blocks, xs){
-  let ys = xs || [];
-  for(let block of blocks){
-    if (config.match.test(block.content)) {
-      ys = ys.concat([block.uuid]);
-    }
-    if (block.children && block.children.length) {
-      ys = getIds(block.children, ys);
+const config = {
+  "buttons": {
+    "todos": {
+      "refreshRate": 5,
+      "hits": ["text-decoration: underline;", ""],
+      "styles": [{
+        "tooltip": "Without closed tasks",
+        "char": "\\e6ed",
+        "hits": "div#main-content-container div[data-refs-self*='\"done\"'], div#main-content-container div[data-refs-self*='\"canceled\"']",
+        "style": "div[data-refs-self*='\"done\"']:not(:focus-within), div[data-refs-self*='\"canceled\"']:not(:focus-within) {display: none;}"
+      },{
+        "tooltip": "With closed tasks",
+        "char": "\\e600",
+        "hits": "div#main-content-container div[data-refs-self*='\"done\"'], div#main-content-container div[data-refs-self*='\"canceled\"']",
+        "style": "div#main-content-container:hover div[data-refs-self*='\"done\"'] span.inline, div#main-content-container:hover div[data-refs-self*='\"canceled\"'] span.inline {text-decoration: underline wavy;}"
+      }]
     }
   }
-  return ys;
-}
+};
 
-async function getJournals(){
-  return config.targets.includes("journals") ? logseq.DB.q("(between -90d today)") : [];
-}
+const state = {};
 
-function getPage(){
-  let tries = 20;
+async function getPage(){
+  let tries = 30;
   return new Promise(function(resolve, reject){
     const iv = setInterval(async function(){
       const page = await logseq.Editor.getCurrentPage();
@@ -41,142 +38,110 @@ function getPage(){
   });
 }
 
-async function getPageBlockRefs(name){
-  const blocks = await logseq.DB.datascriptQuery(`
-    [:find (pull ?block [*])
-    :where
-    [?block :block/page ?p]
-    [?block :block/refs ?ref-page]
-    [?ref-page :block/name "${name}"]]`);
-  return blocks ? blocks.flat().map(function(b){
-    return {...b, uuid: b.uuid.$uuid$}
-  }) : [];
-}
-
-function getPagesBlocksAndRefs(page){
-  let tries = 20;
-  return new Promise(function(resolve, reject){
-    const iv = setInterval(async function(){
-      const [pageBody, linkedRefs] = [
-        config.targets.includes("page-body") ? await logseq.Editor.getPageBlocksTree(page.name) : null,
-        config.targets.includes("linked-refs") ? await getPageBlockRefs(page.name) : null
-      ];
-      const blocks = (pageBody || []).concat(linkedRefs || []);
-      if (blocks) {
-        clearInterval(iv);
-        resolve(blocks);
-      }
-      tries--;
-      if (tries < 0){
-        clearInterval(iv);
-        reject(page);
-      }
-    }, 500);
+function setButton(key, char, tooltip, status){
+  top.document.body.setAttribute(`data-sc-${key}`, status || "");
+  logseq.App.registerUIItem('toolbar', {
+    key: `button-${key}`,
+    template: `
+    <a class="button eye" title="${tooltip || ""}" data-key="${key}" data-on-click="cycle">
+      <i class="ti ti-home"></i>
+    </a>
+    `,
+  });
+  logseq.provideStyle({
+    key: `icon-${key}`,
+    style: `
+    .eye[data-key="${key}"] i:before {
+      content: "${char}";
+    }`
   });
 }
 
-function onPageChanges(refreshRate){
-  const callbacks = [];
-  setInterval(async function(){
-    state.edits.unshift(await logseq.Editor.getCurrentPage());
-    while (state.edits.length > 2) {
-      state.edits.pop();
-    }
-    const [current, prior] = state.edits;
-    if (current?.name === prior?.name && current?.updatedAt !== prior?.updatedAt) {
-      for(callback of callbacks){
-        callback(current);
-      }
-    }
-  }, refreshRate * 1000);
-  return function subscribe(callback){
-    callbacks.push(callback);
+async function cycle(el) {
+  const key = el.dataset.key,
+        st  = state[key],
+        btn = config.buttons[key];
+  st.idx = btn.styles[st.idx + 1] ? st.idx + 1 : 0;
+  await refresh(key, btn, st);
+}
+
+async function refresh(key, btn, state){
+  const {char, tooltip, status, style, hits} = btn.styles[state.idx];
+  state.hits = hits;
+  style && logseq.provideStyle({
+    key: `active-${key}`,
+    style
+  });
+  setButton(key, char, tooltip, status);
+  detectHits(key, btn, state);
+}
+
+function detectHits(key, config, state){
+  if (state.hits) {
+    const el = top.document.querySelector(state.hits);
+    const style = el ? config.hits[0] : config.hits[1];
+    logseq.provideStyle({
+      key: `hits-${key}`,
+      style: `
+      .eye[data-key="${key}"] i:before {
+        ${style};
+      }`
+    });
   }
 }
 
-function refreshStyle(blocks){
-  state.ids = blocks ? getIds(blocks) : [];
-  model.toggle(null);
+function refreshHits(key, config, state){
+  (config.refreshRate || 0) > 0 && setInterval(function(){
+    detectHits(key, config, state);
+  }, config.refreshRate * 1000);
 }
 
 function createModel(){
-  return {
-    toggle(src) {
-      if (src){
-        state.status = state.status == "closed" ? "opened" : "closed";
-      }
-      const closed = (state.ids.length ? state.ids.map(function(id){
-        return `div[blockid="${id}"]`;
-      }).join(", ") : "#wide-eyed") + " " + (state.status === "closed" ? `{${config.closed}}` : "{}");
-      const opened = (state.ids.length ? state.ids.map(function(id){
-        return `div#main-content-container:hover div[blockid="${id}"] div.flex-1 span.inline`;
-      }).join(", ") : "#wide-eyed") + " " + `{${config.opened}}`;
+  return {refresh, cycle};
+}
 
-      const style = [closed, opened].join("\n ");
-      const klass = ["button", state.status, state.ids.length ? "hits" : "empty"].join(" ");
-      logseq.App.registerUIItem('toolbar', {
-        key: 'toggle',
-        template: `
-        <a id="eye" class="${klass}" data-on-click="toggle">
-          <i></i>
-        </a>
-        `,
-      });
-      logseq.provideStyle({
-        key: 'selection',
-        style
-      });
+async function main(){
+  Object.assign(config, logseq.settings);
+
+  logseq.provideStyle({
+    style: `
+    @import url("https://at.alicdn.com/t/font_2409735_haugsknp36e.css");
+    .eye {
+      font-size: 20px;
+    }
+    .eye i:before {
+      font-family: iconfont !important;
+      font-style: normal;
+      color: var(--ls-primary-text-color);
+    }`
+  });
+
+  for (let key in config.buttons) {
+    const btn = config.buttons[key];
+    if (btn.disabled) {
+      delete config.buttons[key];
+    } else {
+      const {char, tooltip, status} = btn.styles[0];
+      state[key] = {idx: 0};
+      setButton(key, char, tooltip, status);
     }
   }
-}
 
-async function main () {
-  Object.assign(config, {
-    refreshRate: logseq.settings.refreshRate || 5,
-    targets: logseq.settings.targets || ["page-body", "linked-refs"],
-    status: logseq.settings.status || "closed",
-    match: new RegExp(logseq.settings.match || "(^DONE|^CANCELED) "),
-    closed: logseq.settings.closed || "display: none;",
-    opened: logseq.settings.opened || "text-decoration: underline wavy;"
-  });
+  await getPage();
 
-  const onPageChanged = config.refreshRate > 0 ? onPageChanges(config.refreshRate) : function(){};
-
-  state.status = config.status;
-
-  logseq.provideStyle(`
-    @import url("https://at.alicdn.com/t/font_2409735_haugsknp36e.css");
-    #eye {
-      padding-bottom: 15px;
-    }
-    #eye i:before {
-      content: "\\e6ed";
-      font-family: iconfont !important;
-      font-size: 22px;
-    }
-    #eye.closed i:before {
-      content: "\\e600";
-    }
-    #eye.hits i:before {
-      text-decoration: underline;
-    }`);
-
-  onPageChanged(async function(page){
-    refreshStyle(await (page ? getPagesBlocksAndRefs(page) : getJournals()));
-  });
+  for (let key in config.buttons) {
+    const btn = config.buttons[key],
+          st  = state[key];
+    await refresh(key, btn, st);
+    refreshHits(key, btn, st);
+  }
 
   logseq.App.onRouteChanged(async function(e){
-    if (["/all-journals", "/"].includes(e.path)) {
-      const blocks = await getJournals();
-      refreshStyle(blocks);
-    } else {
-      const page = await getPage();
-      const blocks = await getPagesBlocksAndRefs(page);
-      refreshStyle(blocks);
+    await getPage();
+    for (let key in config.buttons) {
+      refresh(key, config.buttons[key], state[key]);
     }
   });
 }
 
-const model = createModel();
-
-logseq.ready(model, main).catch(console.error.bind(console));
+logseq.ready(createModel(), main).catch(console.error.bind(console));
